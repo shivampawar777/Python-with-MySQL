96d16342bc0c226d8d5c08afaa8f952d028ae8a8
shivampawar777 <pawarshivam826@gmail.com>
2025-03-08 23:15:10 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import mysql.connector as connector
+
+class Student:
+    def __init__(self):
+        self.con = connector.connect(host="localhost", user="root", password="root", database="Test", port="3306")
+        print("Connected to Database...")
+
+        query = 'create table if not exists Students(SID int primary key, Name varchar[200], City varchar[100])'
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print("Table Students created...")
+
+    def insert(self, id, name, city):
+        query = "insert into Students(SID={}, Name='{}', City='{}')".format(id, name, city)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+        print(id, name, city)
+
+    def update(self,id, name, city):
+        query = "update user set Name='{}', City='{}' where SID={}".format(name, city, id)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+
+        print("Data upadated...")
+
+    def delete(self, id):
+        query = "delete from Students where SID = {}".format(id)
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+
+        print("Data deleted...")
+
+    def fetch_all(self):
+        query = 'select * from Students'
+        cur = self.con.cursor()
+        cur.execute(query)
+        self.con.commit()
+
+        for row in cur:
+            print(row)
+
+        print("***...Data...***")
+
+
+
+# main() function
+S = Student()
+S.insert(123, 'Sandesh', 'Pune')
+S.update(456, 'Ajay','Mumbai')
+S.delete(123,)
+S.fecth_all()
